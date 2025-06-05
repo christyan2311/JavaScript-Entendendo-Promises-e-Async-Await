3d88fb6ad168d6e688c1e90776fbcb665233a5ea
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-05 20:29:31 -0300

    Envio de Arquivos

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const uploadButton = document.getElementById('upload-btn');
+const inptUpload = document.getElementById('image-upload');
+
+uploadButton.addEventListener('click', () => {
+    inptUpload.click();
+});
