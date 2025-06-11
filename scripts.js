b48a65f164b53aeb2d887939b701c95dd2f7302d
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-11 18:15:08 -0300

    Ajustes

--- scripts.js
+++ scripts.js
@@ -1,35 +1,38 @@
-const uploadButton = document.getElementById('upload-btn');
-const inptUpload = document.getElementById('image-upload');
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload")
 
-uploadButton.addEventListener('click', () => {
-    inptUpload.click();
-});
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
 
 function lerConteudoDoArquivo(arquivo) {
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
         leitor.onload = () => {
-            resolve({url : leitor.result, nome: arquivo.name});
-        };
+            resolve({ url: leitor.result, nome: arquivo.name })
+        }
+
         leitor.onerror = () => {
-            reject(`Erro ao ler o arquivo: ${arquivo.name}`);
-        };
-        leitor.readAsDataURL(arquivo);
-    });
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
 }
 
-const imagemPrincipal = document.querySelector('main-imagem');
-const nomeDaImagem = document.querySelector('.container-imagem-nome p');
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
 
-inptUpload.addEventListener('change', async (event) => {
-    const arquivo = event.target.files[0];
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
     if (arquivo) {
         try {
             const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
             imagemPrincipal.src = conteudoDoArquivo.url;
             nomeDaImagem.textContent = conteudoDoArquivo.nome;
-        } catch (error) {
-            console.error(error);
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo")
         }
     }
 })
