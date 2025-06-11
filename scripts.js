19555bf64e536bb90dc715d50bdead9a6b87c976
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-11 17:30:28 -0300

    Promises

--- scripts.js
+++ scripts.js
@@ -4,3 +4,16 @@ const inptUpload = document.getElementById('image-upload');
 uploadButton.addEventListener('click', () => {
     inptUpload.click();
 });
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url : leitor.result, nome: arquivo.name});
+        };
+        leitor.onerror = () => {
+            reject(`Erro ao ler o arquivo: ${arquivo.name}`);
+        };
+        leitor.readAsDataURL(arquivo);
+    });
+}
