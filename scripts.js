76798455b599ad00c21e90099b57fa96a4eeb6e7
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-11 18:06:13 -0300

    Funções assincronas

--- scripts.js
+++ scripts.js
@@ -17,3 +17,19 @@ function lerConteudoDoArquivo(arquivo) {
         leitor.readAsDataURL(arquivo);
     });
 }
+
+const imagemPrincipal = document.getElementById('main-imagem');
+const nomeDaImagem = document.querySelector('.container-imagem-nome p');
+
+inptUpload.addEventListener('change', async (event) => {
+    const arquivo = event.target.files[0];
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+})
