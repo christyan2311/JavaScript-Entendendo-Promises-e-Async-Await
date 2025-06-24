7d10428d071443f5cf6269231ce651784d19fa35
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-24 18:28:18 -0300

    Limitar tags inseridas

--- scripts.js
+++ scripts.js
@@ -40,19 +40,7 @@ inputUpload.addEventListener("change", async (evento) => {
 const inputTags = document.getElementById("input-tags");
 const listaDeTags = document.querySelector(".tags-list");
 
-inputTags = document.getElementById("input-tags");
-inputTags.addEventListener("keypress", (evento) => {
-    if (evento.key === "Enter") {
-        evento.preventDefault();
-        const tagTexto = evento.target.value.trim();
-        if (tagTexto !== "") {
-            const tagNova = document.createElement("li");
-            tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close.svg" class="remove-tag">`;
-            listaDeTags.appendChild(tagNova);
-            evento.target.value = "";
-        }
-    }
-});
+
 
 listaDeTags.addEventListener("click", (evento) => {
     if (evento.target.classList.contains("remove-tag")) {
@@ -69,3 +57,26 @@ async function verificatTagsDisponiveis(tagTexto) {
     }, 1000);
  })
 }
+
+inputTags.addEventListener("keypress", async (evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "") {
+            try {
+                const tagExiste = await verificaTagsDisponiveis(tagTexto);
+                if (tagExiste) {
+                    const tagNova = document.createElement("li");
+                    tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+                    listaTags.appendChild(tagNova);
+                    inputTags.value = "";
+                } else {
+                    alert("Tag não foi encontrada.");
+                }
+            } catch (error) {
+                console.error("Erro ao verificar a existência da tag");
+                alert("Erro ao verificar a existência da tag. Verifique o console.")
+            }
+        }
+    }
+})
